687368d45dfce5dc63f4af65ef4da6b969416784
bhuwan <bkspoudel@gmail.com>
2022-03-23 10:18:04 +0100

    made some types to ApiResponse

--- src/Components/AQI/InfoAQI.tsx
+++ src/Components/AQI/InfoAQI.tsx
@@ -1,105 +1,112 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import { WeatherInfo } from "./InfoDisplayAQI";
 
 const Wrapper = styled.div`
-	display: flex;
-	flex-flow: column;
-	flex: 1 1;
-	align-items: center;
+  display: flex;
+  flex-flow: column;
+  flex: 1 1;
+  align-items: center;
 `;
 
 const Wrapper1 = styled.div`
-	display: flex;
-	flex-flow: row;
-	background: #b0e867;
-	width: 95%;
-	border-radius: 8px;
-	justify-content: space-around;
-	align-items: center;
-	font-size: 1.5rem;
+  display: flex;
+  flex-flow: row;
+  background: #b0e867;
+  width: 95%;
+  border-radius: 8px;
+  justify-content: space-around;
+  align-items: center;
+  font-size: 1.5rem;
 `;
 
 const BackgroundWeather = styled.div`
-	background: black;
-	width: 100%;
-	height: 100%;
-	display: flex;
-	flex-flow: column;
-	justify-content: space-around;
-	align-items: center;
-	padding-bottom: 10px;
+  background: black;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-flow: column;
+  justify-content: space-around;
+  align-items: center;
+  padding-bottom: 10px;
 `;
 
 const Wrapper2 = styled(Wrapper1)`
-	background: #49739a;
-	margin: 20px;
+  background: #49739a;
+  margin: 20px;
 `;
 
-function InfoAQI({ data }) {
-	return (
-		<Wrapper>
-			<BackgroundWeather>
-				<h1 style={{ color: 'white' }}>Helsinki Air Quality Index</h1>
-				<Wrapper1>
-					<div style={{ flex: '2 1', marginBottom: '15px' }}>
-						<h1>{data && data.aqi}</h1>
-						<strong>
-							<p style={{ fontSize: '1rem' }}>AQI</p>
-						</strong>
-					</div>
-					<div
-						style={{
-							flex: '1 1',
-							marginTop: '3rem',
-							display: 'flex',
-							flexFlow: 'column',
-							justifyContent: 'center',
-						}}>
-						{data && data.pm10.toFixed(2)}
-						<strong>
-							<p>PM 10 (µg/m³)</p>
-						</strong>
-					</div>
-					<div
-						style={{
-							flex: '1 1',
-							marginTop: '3rem',
-							display: 'flex',
-							flexFlow: 'column',
-							justifyContent: 'center',
-						}}>
-						{data && data.pm25.toFixed(2)}
-						<strong>
-							<p>PM 2.5 (µg/m³)</p>
-						</strong>
-					</div>
-				</Wrapper1>
-			</BackgroundWeather>
+type InfoAQIProps = {
+  data: WeatherInfo;
+};
 
-			<Wrapper2>
-				<p>
-					NO<span style={{ fontSize: '1rem' }}>2</span>
-					<br />
-					{data && data.no2.toFixed(2)}
-				</p>
-				<p>
-					CO
-					<br />
-					{data && data.co.toFixed(2)}
-				</p>
-				<p>
-					O<span style={{ fontSize: '1rem' }}>3</span>
-					<br />
-					{data && data.o3.toFixed(2)}
-				</p>
-				<p>
-					SO<span style={{ fontSize: '1rem' }}>2</span>
-					<br />
-					{data && data.so2.toFixed(2)}
-				</p>
-			</Wrapper2>
-		</Wrapper>
-	);
+function InfoAQI({ data }: InfoAQIProps) {
+  return (
+    <Wrapper>
+      <BackgroundWeather>
+        <h1 style={{ color: "white" }}>Helsinki Air Quality Index</h1>
+        <Wrapper1>
+          <div style={{ flex: "2 1", marginBottom: "15px" }}>
+            <h1>{data && data.aqi}</h1>
+            <strong>
+              <p style={{ fontSize: "1rem" }}>AQI</p>
+            </strong>
+          </div>
+          <div
+            style={{
+              flex: "1 1",
+              marginTop: "3rem",
+              display: "flex",
+              flexFlow: "column",
+              justifyContent: "center",
+            }}
+          >
+            {data && data.pm10.toFixed(2)}
+            <strong>
+              <p>PM 10 (µg/m³)</p>
+            </strong>
+          </div>
+          <div
+            style={{
+              flex: "1 1",
+              marginTop: "3rem",
+              display: "flex",
+              flexFlow: "column",
+              justifyContent: "center",
+            }}
+          >
+            {data && data.pm25.toFixed(2)}
+            <strong>
+              <p>PM 2.5 (µg/m³)</p>
+            </strong>
+          </div>
+        </Wrapper1>
+      </BackgroundWeather>
+
+      <Wrapper2>
+        <p>
+          NO<span style={{ fontSize: "1rem" }}>2</span>
+          <br />
+          {data && data.no2.toFixed(2)}
+        </p>
+        <p>
+          CO
+          <br />
+          {data && data.co.toFixed(2)}
+        </p>
+        <p>
+          O<span style={{ fontSize: "1rem" }}>3</span>
+          <br />
+          {data && data.o3.toFixed(2)}
+        </p>
+        <p>
+          SO<span style={{ fontSize: "1rem" }}>2</span>
+          <br />
+          {data && data.so2.toFixed(2)}
+        </p>
+      </Wrapper2>
+    </Wrapper>
+  );
 }
 
 export default InfoAQI;
--- src/Components/AQI/InfoDisplayAQI.tsx
+++ src/Components/AQI/InfoDisplayAQI.tsx
@@ -1,42 +1,55 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import axios from 'axios';
-import InfoAQI from './InfoAQI';
+import React, { Component } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import InfoAQI from "./InfoAQI";
 
 const Display = styled.div`
-	min-height: 45vh;
-	min-width: 25vw;
-	background: white;
-	opacity: 0.8;
-	margin: 20px;
-	border-radius: 8px;
-	opacity: 0.8;
-	text-align: center;
-	display: flex;
-	flex-flow: column;
+  min-height: 45vh;
+  min-width: 25vw;
+  background: white;
+  opacity: 0.8;
+  margin: 20px;
+  border-radius: 8px;
+  opacity: 0.8;
+  text-align: center;
+  display: flex;
+  flex-flow: column;
 `;
+export type WeatherInfo = {
+  aqi: number;
+  pm10: number;
+  pm25: number;
+  no2: number;
+  co: number;
+  o3: number;
+  so2: number;
+};
+type ApiResponse = {
+  count: number;
+  data: WeatherInfo[];
+};
 
 class InfoDisplayAQI extends Component {
-	state = {
-		aqi: null,
-	};
-	componentDidMount() {
-		axios
-			.get(
-				'https://api.weatherbit.io/v2.0/current/airquality?city=Helsinki&key=12b3830093e741069ba73debc71898a8'
-			)
-			.then((res) => this.setState({ aqi: res.data.data[0] }));
-	}
+  state = {
+    aqi: null,
+  };
+  componentDidMount() {
+    axios
+      .get<ApiResponse>(
+        "https://api.weatherbit.io/v2.0/current/airquality?city=Helsinki&key=12b3830093e741069ba73debc71898a8"
+      )
+      .then((res) => this.setState({ aqi: res.data.data[0] }));
+  }
 
-	render() {
-		return (
-			<div>
-				<Display>
-					<InfoAQI data={this.state.aqi} />
-				</Display>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <Display>
+          <InfoAQI data={this.state.aqi} />
+        </Display>
+      </div>
+    );
+  }
 }
 
 export default InfoDisplayAQI;
